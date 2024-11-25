4d1e3f025c4bc6e523fff86d403e72deb1655c60
Tam Nguyen <tamnva@gmail.com>
2024-11-25 14:42:33 +0100

    fix: change np.Inf to np.inf

--- hydroecolstm/train/trainer.py
+++ hydroecolstm/train/trainer.py
@@ -232,7 +232,7 @@ class EarlyStopping:
         self.counter = 0
         self.best_score = None
         self.early_stop = False
-        self.val_loss_min = np.Inf
+        self.val_loss_min = np.inf
         self.delta = delta
         self.trace_func = trace_func
     def __call__(self, val_loss, model):
