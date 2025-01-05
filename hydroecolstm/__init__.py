7b0a000e568ce85a4dcca6cafd067aa23e76b92a
Tam Nguyen <tamnva@gmail.com>
2025-01-05 12:57:37 +0100

    doc: increase version

--- hydroecolstm/__init__.py
+++ hydroecolstm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __author__ = 'Tam V. Nguyen'
 __credits__ = 'Helmholtz Centre for Environmental Research (UFZ)'
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with readme_file.open("r") as fp:
     
 setup(
     name='hydroecolstm',
-    version='0.3.3',    
+    version='0.3.4',    
     description='A python package for HydroEcological Modelling using LSTM',
     long_description=long_description,
     long_description_content_type='text/markdown',
